2010d97f39508b7332bb27e898b7bb3e54e67a86
fiascomatthew <fiasco.matthew@gmail.com>
2025-02-06 22:43:00 +0100

    add error page, css and error handling middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import router  from './routers/router';
+import notFound  from './middlewares/notFound';
+import errorHandler  from './middlewares/errorHandler';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,10 +21,11 @@ app.use(express.static(path.join(__dirname, "public")));
 // Favicon static route
 app.use("/favicon.ico", express.static(path.join(__dirname, "public", "images", "logo.png")));
 
-app.get("/", (req: Request, res: Response) => {
-  // res.send("SkillSwap is launched !");
-  res.render("pages/home");
-});
+app.use(router);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
