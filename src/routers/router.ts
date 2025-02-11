bae7b33d4118b366f107b4b2caa37c26991e5d15
fiascomatthew <fiasco.matthew@gmail.com>
2025-02-11 11:05:20 +0100

    reorganize router.ts

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -8,11 +8,11 @@ const router = Router();
 router.get('/', catchErrors(getHomePage));
 router.get('/search', catchErrors(getSkillsSearch));
 
+router.post('/login', catchErrors(authController.login));
+router.post('/register', catchErrors(authController.register));
+
 router.get('/error', (req: Request, res: Response) => {
   res.render('pages/error', { message: 'Une erreur est survenue' });
 });
-
-router.post('/login', catchErrors(authController.login));
-router.post('/register', catchErrors(authController.register));
 
 export default router;
