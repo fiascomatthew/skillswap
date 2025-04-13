cab6b2e2ab5ae7365a44616fb0ca9d124e9b8aaf
fiascomatthew <fiasco.matthew@gmail.com>
2025-04-13 17:01:34 +0200

    add new toggle route for following and unfollowing a user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,6 +5,7 @@ import HttpError from '../errors/HttpError';
 export const userController = {
   async show(req: Request, res: Response, next: NextFunction) {
     const { id } = req.params;
+    const userId = req.session.connectedUser?.id;
 
     const user = await User.findByPk(id, {
       include: [
@@ -28,6 +29,29 @@ export const userController = {
       return next(new HttpError('Utilisateur introuvable', 404));
     }
 
-    return res.render('pages/user', { user });
+    const currentUser = await User.findByPk(userId);
+    const isFollowing = currentUser ? await currentUser.isFollowing(user) : false;
+
+    return res.render('pages/user', { user, isFollowing });
+  },
+
+  async toggleFollow(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
+    const userId = req.session.connectedUser?.id;
+
+    const user = await User.findByPk(id);
+    const currentUser = await User.findByPk(userId);
+
+    if (!user || !currentUser) {
+      return res.status(404).json({ success: false, message: 'Utilisateur introuvable' });
+    }
+
+    await ((await currentUser.isFollowing(user))
+      ? user.removeFollower(currentUser)
+      : user.addFollower(currentUser));
+
+    console.log('isFollowing:', await currentUser.isFollowing(user));
+
+    return res.redirect(`/users/${id}`);
   },
 };
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,13 @@
-import { Model, Table, Column, DataType, AllowNull, Unique, Validate } from 'sequelize-typescript';
+import {
+  Model,
+  Table,
+  Column,
+  DataType,
+  AllowNull,
+  Unique,
+  Validate,
+  BelongsToMany,
+} from 'sequelize-typescript';
 
 @Table({
   tableName: 'user',
@@ -22,4 +31,25 @@ export class UserModel extends Model<UserModel> {
   location!: string;
   @Column(DataType.TEXT)
   bio!: string;
+
+  @BelongsToMany(() => UserModel, 'user_has_follower', 'user_id', 'follower_id')
+  followers?: UserModel[];
+  @BelongsToMany(() => UserModel, 'user_has_follower', 'follower_id', 'user_id')
+  following?: UserModel[];
+
+  async isFollowing(user: UserModel): Promise<boolean> {
+    const following = await this.$get('following', { where: { id: user.id } });
+    return following && following.length > 0;
+  }
+
+  async addFollower(user: UserModel): Promise<void> {
+    console.log('USER:', user);
+    console.log('THIS:', this);
+    console.log('FOLLOWERS:', this.followers);
+    await this.$add('followers', user);
+  }
+
+  async removeFollower(user: UserModel): Promise<void> {
+    await this.$remove('followers', user);
+  }
 }
