08198517b5fb3b8e5d8aa03bc10ca850adc952f2
fiascomatthew <fiasco.matthew@gmail.com>
2025-04-14 15:22:22 +0200

    add dahsboard route and corresponding method in dashboardController

---
+++ src/controllers/dashboardController.ts
@@ -0,0 +1,27 @@
+import type { NextFunction, Request, Response } from 'express';
+import { User } from '../models';
+import HttpError from '../errors/HttpError';
+
+export const dashboardController = {
+  async show(req: Request, res: Response, next: NextFunction) {
+    const userId = req.session.connectedUser?.id;
+
+    if (!userId) {
+      return next(new HttpError('Utilisateur non connecté', 401));
+    }
+
+    const user = await User.findByPk(userId, {
+      include: [
+        { association: 'skills' },
+        { association: 'interests' },
+        { association: 'availabilities' },
+      ],
+    });
+
+    if (!user) {
+      return next(new HttpError('Utilisateur introuvable', 404));
+    }
+
+    return res.render('pages/dashboard', { user });
+  },
+};
